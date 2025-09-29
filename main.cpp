9a418cf3e4a75bd21f394594ec8e131988a90629
Maksym Hrechanyi <maksym.hrechanyi.shi.2025@lpnu.ua>
2025-09-29 19:15:13 +0300

    Adde Hello, Maksym

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(){
+    std::cout << "Hello, Maksym" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello Ukraine!" << std::endl;
     std::cout << "Hello Andrii!" << std::endl;
